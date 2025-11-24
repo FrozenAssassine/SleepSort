78a48c0ce1d2e83ec330925f200b150d2d549ea9
Finn Freitag <finn-freitag@web.de>
2025-11-24 22:19:50 +0100

    Encapsulate sleep-sort logic into a dedicated function
    - Moved max/maxNeg computation and timeout-based sorting into SleepSort()
    - Eliminated reliance on outer-scope variables
    - Function now returns a Promise resolving with the sorted result
    - Updated call site to use SleepSort(test).then(...)

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
-let items = [5, 9, 1, 20, 3, -1, -9, 12, 44, 7, 0, 18, -4, 23, 81, -12, 6, 55, 90, -33, 17, 29, 4, -8, 39, 72, 11, -2, 8, 100, 63, 14, -17, 27, 32, -22, 19, 2, 47, 76, -5, 31, 54, -6, 13, 99, 25, -15, 22, 16, -3, 40, 77, 1, -10, 33, 21, 48, -7, 26, 66, 58, 42, -20, 34, 3, 7, 15, -11, 36, 62, 41, 28, -19, 59, 50, 24, 10, -13, 64, 73, 52, 30, -14, 46, 71, 57, 35, -16, 60, 45, 53, 38, -18, 67, 56, 43];
+let test = [5, 9, 1, 20, 3, -1, -9, 12, 44, 7, 0, 18, -4, 23, 81, -12, 6, 55, 90, -33, 17, 29, 4, -8, 39, 72, 11, -2, 8, 100, 63, 14, -17, 27, 32, -22, 19, 2, 47, 76, -5, 31, 54, -6, 13, 99, 25, -15, 22, 16, -3, 40, 77, 1, -10, 33, 21, 48, -7, 26, 66, 58, 42, -20, 34, 3, 7, 15, -11, 36, 62, 41, 28, -19, 59, 50, 24, 10, -13, 64, 73, 52, 30, -14, 46, 71, 57, 35, -16, 60, 45, 53, 38, -18, 67, 56, 43];
 
-let maxNeg = 0;
-let max = 0;
-for (let item of items) {
-    if (item > max) {
-        max = item;
-    }
-    if (item < 0) {
-        if (item < maxNeg) {
-            maxNeg = item;
+SleepSort(test).then((r)=>{
+    console.log(r);
+});
+
+function SleepSort(items) {
+    return new Promise(resolve => {
+        let maxNeg = 0;
+        let max = 0;
+
+        for (let item of items) {
+            if (item > max) max = item;
+            if (item < maxNeg) maxNeg = item;
         }
-    }
+
+        let res = [];
+
+        for (let item of items) {
+            const delay = item < 0
+                ? -(maxNeg - item)
+                : item + -maxNeg;
+
+            setTimeout(() => {
+                res.push(item);
+                if (res.length === items.length) {
+                    resolve(res);
+                }
+            }, delay);
+        }
+    });
 }
-
-let res = [];
-for (let item of items) {
-    if (item < 0) {
-        setTimeout(() => res.push(item), -(maxNeg - item));
-    }
-    else {
-        setTimeout(() => res.push(item), item + -maxNeg);
-    }
-}
-
-
-setTimeout(() => console.log(res), max + -maxNeg + 1);
